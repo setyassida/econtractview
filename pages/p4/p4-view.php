ba9542d83e5a2ba092775e6fd4579694c71212c7
luqmanahmads <luqman_ahmads@ymail.com>
2017-04-27 17:03:18 +0700

    update

--- pages/p4/p4-view.php
+++ pages/p4/p4-view.php
@@ -199,192 +199,182 @@
     <section class="content-header">
       <h1>
         Harga Perkiraan Sendiri
-        <small>Buat HPS Baru</small>
+        <small>0001/HPS/2017</small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
         <li class="active"><a href="p4.php">Harga Perkiraan Sendiri</a></li>
-        <li>Create</li>
+        <li>0001/HPS/2017</li>
       </ol>
     </section>
 
-    <!-- Main content -->
-
     <section class="content">
-      <!-- SELECT2 EXAMPLE -->
-      <form class="form-horizontal">
-      <div class="box box-danger">
-        <div class="box-header with-border">
-          <div class="row">
-            <div class="col-md-12">
-              <h4>Buat HPS Baru</h4>
+      <div class="row">
+        <div class="col-md-3">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="fa fa-user"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Approver 1</span>
+              <strong>Sarah Connor</strong>
+              <br><br>
+              <div class="text-right text-success"><i class="fa fa-check"></i></div>
             </div>
+            <!-- /.info-box-content -->
           </div>
-
         </div>
-        <!-- /.box-header -->
-        <div class="box-body">
-          
-            <div class="form-group">
-              <label class="control-label col-md-2">Nomor Kontrak :</label>
-              <div class="col-md-9">
-                <span class="col-md-3">
-                  <input type="text" class="form-control input-sm" placeholder="LOOKUP..." required>
-                </span>
-              </div>
-            </div>
+        <div class="col-md-3">
+          <div class="info-box">
+            <span class="info-box-icon bg-green"><i class="fa fa-user"></i></span>
 
-            <div class="col-md-12">
-              <div class="box box-solid">
-                <div class="box-header ">
-                 
-                  <div class="callout callout-danger">
-                    <h5><span class="glyphicon glyphicon-info-sign"></span>  Informasi Kontrak</h5>
+            <div class="info-box-content">
+              <span class="info-box-text">Approver 2</span>
+              <strong>Katniss Everdeen</strong>
+              <br><br>
+              <div class="text-right text-success"><i class="fa fa-check"></i></div>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="info-box">
+            <span class="info-box-icon bg-gray"><i class="fa fa-user"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Approver 3</span>
+              <strong>Peeta Mellark</strong>
+              <br><br>
+              <div class="text-right text-success"><i class="fa fa-clock-o"></i></div>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="info-box">
+            <span class="info-box-icon bg-gray"><i class="fa fa-user"></i></span>
+
+            <div class="info-box-content">
+              <span class="info-box-text">Approver 4</span>
+              <strong>Jack Sparrow</strong>
+              <br><br>
+              <div class="text-right text-success"><i class="fa fa-clock-o"></i></div>
+            </div>
+            <!-- /.info-box-content -->
+          </div>
+        </div>
+      </div>
+
+      <div class="form-horizontal">
+        <div class="box box-danger">
+          <div class="box-header with-border">
+            <h4>Informasi HPS</h4>
+          </div>
+          <div class="box-content">
+            <form class="form-horizontal">
+
+              
+                
+                <div class="col-md-6">
+                  <div class="form-group">
+                    <label class="control-label col-md-4">Nomor HPS :</label>
+                    <div class="form-control-static col-md-8">
+                      0001/HPS/2017
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="control-label col-md-4">Tanggal Opening :</label>
+                    <div class="form-control-static col-md-8">
+                      01-11-2017
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="control-label col-md-4">Nomor Kontrak :</label>
+                    <div class="form-control-static col-md-8">
+                      <a href="#">CA-17001</a>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="control-label col-md-4">Judul Pekerjaan :</label>
+                    <div class="form-control-static col-md-8">
+                      PEMELIHARAAN INFRASTRUKTUR
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="control-label col-md-4">Durasi Pekerjaan :</label>
+                    <div class="form-control-static col-md-8">
+                      50 Hari
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="control-label col-md-4">Jenis Jasa :</label>
+                    <div class="form-control-static col-md-8">
+                      Jasa Pemborongan Pekerjaan, Jasa Penyedia Tenaga Kerja
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="control-label col-md-4">Klasifikasi Pemilihan :</label>
+                    <div class="form-control-static col-md-8">
+                      nilai antara bidang M-Z
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label class="control-label col-md-4">Bobot Nilai CO :</label>
+                    <div class="form-control-static col-md-8">
+                      
+                    </div>
                   </div>
                 </div>
-                <div class="box-body">
-                  
-                  <div class="col-md-6">
-                    <div class="box box-solid">
-                      <div class="box-body">
-                        <div class="form-group">
-                          <label class="control-label col-md-4">Metode Pengadaan :</label>
-                          <div class="form-control-static col-md-8">
-                            Pemilihan Langsung
-                          </div>
-                        </div>
 
-                        <div class="form-group">
-                          <label class="control-label col-md-4">Kontrak Rutin :</label>
-                          <div class="form-control-static col-md-8">
-                            Ya
-                          </div>
-                        </div>
-                        
-                        <div class="form-group">
-                          <label class="control-label col-md-4">Jenis Kontrak :</label>
-                          <div class="form-control-static col-md-8">
-                            Lump Sum (LS)/Harga Satuan (HS)/Gabungan LS & HS
-                          </div>
-                        </div>
+                <div class="col-md-6">
+                  <div style="margin-top:20px">
+                  </div>
+                  <div class="box box-solid" >
+                    <div class="box-body">
+                      <p class="text-muted well well-sm no-shadow" style="margin-top: 10px;">
+                        Nilai HPS yang ditentukan oleh Estimator    
+                      </p>
 
-                        <div class="form-group">
-                          <label class="control-label col-md-4">Klasifikasi Pemilihan :</label>
-                          <div class="form-control-static col-md-8">
-                            nilai antara bidang M-Z
-                          </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-4">HPS Minimum :</label>
+                        <div class="form-control-static col-md-8">
+                          Rp 1.000.000.000,00
                         </div>
-
-                        <div class="form-group">
-                          <label class="control-label col-md-4">Kualifikasi :</label>
-                          <div class="form-control-static col-md-8">
-                            K (kecil) atau NK (non-kecil)
-                          </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-4">HPS Most Likely :</label>
+                        <div class="form-control-static col-md-8">
+                          Rp 3.500.000.000,00
                         </div>
-
-                        <div class="form-group">
-                          <label class="control-label col-md-4">Tingkat Risiko :</label>
-                          <div class="form-control-static col-md-8">
-                            Rendah, Sedang, atau Tinggi
-                          </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-4">HPS Maximum :</label>
+                        <div class="form-control-static col-md-8">
+                          Rp 5.000.000.000,00
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label class="control-label col-md-4">Dokumen Pendukung HPS :</label>
+                        <div class="form-control-static col-md-8">
+                          <button class="btn btn-default btn-xs" style="margin-right:5px"><span class="glyphicon glyphicon-download-alt"></span></button>sample_document.docx
                         </div>
                       </div>
                     </div>
                   </div>
-
-                  <div class="col-md-6">
-                    <div class="box box-solid">
-                      <div class="box-body">
-                        <label class="control-label">Dokumen Terkait Contract Order :</label>
-                        <div style="height:20px"></div>
-                        <table class="table table-bordered table-condensed table-hover">
-                          <thead>
-                            <tr>
-                              <th>Judul Dokumen</th>
-                              <th style="width:75px">Download</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td>Dokumen Risalah Pre-Bid Meeting</td>
-                              <td>
-                                <div class="text-center">
-                                  <button type="button" class="btn btn-xs btn-default" >
-                                    <span class="glyphicon glyphicon-download-alt"></span>
-                                  </button>
-                                </div>
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>Dokumen Breakdown Penawaran</td>
-                              <td>
-                                <div class="text-center">
-                                  <button type="button" class="btn btn-xs btn-default" >
-                                    <span class="glyphicon glyphicon-download-alt"></span>
-                                  </button>
-                                </div>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                      </div>
-                    </div>  
-                  </div>
-
                 </div>
-              </div>
-            </div>
+                <div class="clear-fix"></div>
 
-            <div class="col-md-12">
-              <div class="form-group"> 
-                <label class="control-label col-md-2">HPS Minimum (Rp) :</label>
-                <div class="col-md-10">
-                  <span class="col-md-3">
-                    <input type="number" class="form-control input-sm" required>
-                  </span>
-                </div>
-              </div>
-              <div class="form-group"> 
-                <label class="control-label col-md-2">HPS Most Likely (Rp) :</label>
-                <div class="col-md-10">
-                  <span class="col-md-3">
-                    <input type="number" class="form-control input-sm" required>
-                  </span>
-                </div>
-              </div>
-              <div class="form-group"> 
-                <label class="control-label col-md-2">HPS Maximum (Rp) :</label>
-                <div class="col-md-10">
-                  <span class="col-md-3">
-                    <input type="number" class="form-control input-sm" required>
-                  </span>
-                </div>
-              </div>
-              <div class="form-group"> 
-                <label class="control-label col-md-2">Dokumen Pendukung :</label>
-                <div class="col-md-10">
-                  <span class="col-md-3">
-                    <input type="file" class="input-sm" required>
-                  </span>
-                </div>
-              </div>
+            </form>
+          </div>
+          <div class="box-footer">
 
-              
-            </div>
-
-          
-        </div>
-        <!-- /.box-body -->
-
-        <div class="box-footer">
-          <button type="submit" class="btn btn-success btn-sm pull-right"><span class="glyphicon glyphicon-plus"></span> Create</button>
-          
-          <a href="p4.php"><button type="button" class="btn btn-default btn-sm pull-right" style="margin-right: 5px;"> Back</button></a>
+            <button type="button" class="btn btn-success btn-sm pull-right">Approve</button> 
+            <button type="button" class="btn btn-danger btn-sm pull-right" style="margin-right:5px">Reject</button> 
+            <button type="button" class="btn btn-default btn-sm" style="margin-right:5px"><span class="glyphicon glyphicon-download-alt" style=""></span> Download Template HPS Ini</button> 
+            <button type="button" class="btn btn-default btn-sm" style="margin-right:5px"><span class="glyphicon glyphicon-cloud-upload"></span> Upload Hasil Scan</button>
+          </div>
         </div>
       </div>
-      </form>
-      <!-- /.box -->
-  </section>
+    </section>
 
     <!-- /.content -->
   </div>
