00faf1364153af207f07cc1d032adbebf76edc51
KRINTIL-PC\Dwika <dwikasm@gmail.com>
2017-05-04 17:17:36 +0700

    minor fix

--- pages/p3p/p3pheader.php
+++ pages/p3p/p3pheader.php
@@ -110,14 +110,14 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="../p1/p1.php"><i class="fa fa-circle-o"></i> Contract</a></li>
-            <li><a href="../p2/p2a.php"><i class="fa fa-circle-o"></i> Bidder List</a></li>
+            <!-- <li><a href="../p1/p1.php"><i class="fa fa-circle-o"></i> Contract</a></li> -->
+            <!-- <li><a href="../p2/p2a.php"><i class="fa fa-circle-o"></i> Bidder List</a></li> -->
             <li><a href="../p3/p3.php"><i class="fa fa-circle-o"></i> Prebid Meeting</a></li>
             <li class="active"><a href="p3p.php"><i class="fa fa-circle-o"></i> Penawaran</a></li>
-            <li><a href="../p4/p4.php"><i class="fa fa-circle-o"></i> Harga Perkiraan Sendiri</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Evaluasi Administrasi</a></li>
-             <li><a href="../p6/p6.php"><i class="fa fa-circle-o"></i> Evaluasi Teknis</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Evaluasi Komersial</a></li>
+            <!-- <li><a href="../p4/p4.php"><i class="fa fa-circle-o"></i> Harga Perkiraan Sendiri</a></li> -->
+            <!-- <li><a href="#"><i class="fa fa-circle-o"></i> Evaluasi Administrasi</a></li> -->
+            <!-- <li><a href="../p6/p6.php"><i class="fa fa-circle-o"></i> Evaluasi Teknis</a></li> -->
+            <!-- <li><a href="#"><i class="fa fa-circle-o"></i> Evaluasi Komersial</a></li> -->
             <li><a href="../p9/p9.php"><i class="fa fa-circle-o"></i> LoA</a></li>
             <li><a href="#"><i class="fa fa-circle-o"></i> Kick-of-meeting</a></li>
             <li><a href="#"><i class="fa fa-circle-o"></i> LoI</a></li>     
