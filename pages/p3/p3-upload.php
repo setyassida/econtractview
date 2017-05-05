49f8f351c993370bbef615db1ccb2938dcd5bfb9
luqmanahmads <luqman_ahmads@ymail.com>
2017-05-05 14:36:31 +0700

    hpshpshsphpshsphsphps

--- pages/p3/p3-upload.php
+++ pages/p3/p3-upload.php
@@ -154,9 +154,9 @@
             <li><a href="../p2/p2a.php"><i class="fa fa-circle-o"></i> Bidder List</a></li>
             <li class="active"><a href="../p3/p3.php"><i class="fa fa-circle-o"></i> Prebid Meeting</a></li>
             <li><a href="../p4/p4.php"><i class="fa fa-circle-o"></i> Harga Perkiraan Sendiri</a></li>
-            <li><a href="#"><i class="fa fa-circle-o"></i> Evaluasi Administrasi</a></li>
+            <li><a href="../p5/p5.php"><i class="fa fa-circle-o"></i> Evaluasi Administrasi</a></li>
             <li><a href="../p6/p6.php"><i class="fa fa-circle-o"></i> Evaluasi Teknis</a></li>
-            <li><a href=""><i class="fa fa-circle-o"></i> Evaluasi Komersial</a></li>
+            <li><a href="../p8/eval.php"><i class="fa fa-circle-o"></i> Evaluasi Komersial</a></li>
             <li><a href="../p9/p9.php"><i class="fa fa-circle-o"></i> LoA</a></li>
             <li><a href="../p10/p10.php"><i class="fa fa-circle-o"></i> Kick-of-meeting</a></li>
             <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> LoI</a></li>     
